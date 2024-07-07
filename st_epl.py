1125f0f335808b2ca58e563985b54e4ba374268f
Sokisati <yagizalpersoy@gmail.com>
2024-07-07 11:39:27 +0300

    ip fix

--- st_epl.py
+++ st_epl.py
@@ -1,7 +1,7 @@
 from device_file import *
 
-shell = DistantDevice('192.168.33.23',12345,4);
-groundStation = DistantDevice('192.168.33.243',12346,10);
+shell = DistantDevice('192.168.138.23',12345,4);
+groundStation = DistantDevice('192.168.138.243',12346,10);
 
 #I know it's not distant nor a device but it just fits with the structure
 cameraFilter = DistantDevice('127.0.0.1',12347,2);
