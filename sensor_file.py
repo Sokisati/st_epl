78243092198b09c8c811634363637c29da9720e1
Sokisati <yagizalpersoy@gmail.com>
2024-07-24 22:45:18 +0300

    refactor for date and time

--- sensor_file.py
+++ sensor_file.py
@@ -152,7 +152,7 @@ class SensorDataPack:
         self.roll = 0;
         self.pitch = 0;
         self.yaw = 0;
-        self.dateAndTime = '1/1/2038-00/00/00'
+        self.dateAndTime = '1/1/2038-31/52/13'
         
 class SensorPack:
     
